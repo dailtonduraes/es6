4a9f7dd256955189db5ab854ecaf13f0c69fab45
Dailton Durães <dailtonsd10@gmail.com>
2019-11-21 16:59:23 -0300

    main alterado arrays ES6

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const nome = 'Dailton'
+/*const nome = 'Dailton'
 const idade = 35;
 
 const usuario = {
@@ -7,4 +7,35 @@ const usuario = {
     empresa:'CPC INFORMÁTICAAA'
 }
 
-console.log(usuario);
+console.log(usuario);*/
+
+const arr = [1,2,3,4, 30];
+
+const newArr = arr.map(function(item){
+    return item * 2
+});
+
+
+const sum = arr.reduce(function(total, next){
+    return total + next;
+})
+
+
+const arrPares = arr.filter(function(item){
+    return (item % 2 === 0);
+})
+
+const find = arr.find(function(item){
+    return item === 5;
+})
+
+
+
+
+console.log(arrPares);
+
+console.log(sum);
+
+console.log(newArr);
+
+console.log(find);
